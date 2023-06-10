0c9271c3b7153cbe4994ffb911074066de7d6666
TarenP <63258880+TarenP@users.noreply.github.com>
2023-06-10 16:56:04 -0700

    s

--- DataManager/manager.py
+++ DataManager/manager.py
@@ -8,9 +8,23 @@ from flask_socketio import SocketIO, emit
 from os.path import exists
 import subprocess
 
+import os
+import logging
+from flask import jsonify
+from flask_executor import Executor
+import openai
+
+
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import storage
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+
+# Load your OpenAI API key
+openai.api_key = "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"
+
 
 def csv_to_json(csvFilePath, jsonFilePath):
     jsonArray = []
@@ -43,8 +57,46 @@ def post_json(url, data):
     # Print the response content
     print(response.json())
 
+def generate_prompt(user_message):
+    # Analyze the user_message and generate a suitable system message.
+    if 'math' in user_message.lower():
+        return "You are a helpful tutor specializing in Math."
+    elif 'science' in user_message.lower():
+        return "You are a helpful tutor specializing in Science."
+    elif 'english' in user_message.lower():
+        return "You are a helpful tutor specializing in English."
+    else:
+        return "You are a helpful tutor."
 
-# # Initialize the Firebase Admin SDK
+def generate_response(msg):
+    try:
+        system_message = generate_prompt(msg)
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": system_message},
+                {"role": "user", "content": msg}
+            ]
+        )
+        # Return the AI's response as JSON
+        return jsonify({'message': response['choices'][0]['message']['content']})
+    except Exception as e:
+        logging.error(f"Error generating response: {e}")
+        return jsonify({'message': "Sorry, I'm having trouble generating a response right now."})
+
+def append_to_json_file(filepath, data):
+    try:
+        with open(filepath, "r+") as file:
+            file_data = json.load(file)
+            file_data.append(data)
+            file.seek(0)
+            json.dump(file_data, file)
+    except FileNotFoundError:
+        with open(filepath, "w") as file:
+            json.dump([data], file)
+
+
+# Initialize the Firebase Admin SDK
 # cred = credentials.Certificate("path/to/serviceAccountKey.json")
 # firebase_admin.initialize_app(cred)
 
@@ -85,8 +137,38 @@ def receive_data():
         jsonFilePath = r"DataManager/Data/class" + classId +".json"
 
         csv_to_json(csvFilePath, jsonFilePath)
-        
+        bucket = storage.bucket()
+
+        # Specify the local file path and desired remote file name
+        local_file_path = "DataManager/Data/class01.json"
+        remote_file_name = "files/file.txt"
+
+        blob = bucket.blob(remote_file_name)
+        blob.upload_from_filename(local_file_path)
+
+        print("File uploaded successfully.")
         return "Invalid data"
+
+@app.route('/')
+def index():
+    return "Chat Interface"
+
+@app.route('/message', methods=['POST'])
+def handle_message():
+    user_message = request.json['message']
+    logging.info('Received message: ' + user_message)
+    if user_message.startswith("/gpt"):
+        # Extract the question for GPT
+        gpt_question = user_message[5:]  # Skip the "/gpt " part
+        response = generate_response(gpt_question)
+        # Append the question and response to the chat log
+
+        append_to_json_file("DataManager/Data/class01.json", {"question": gpt_question, "response": response.json['message']})
+        return response
+    else:
+        # If it's not a /gpt command, just return a generic response
+        return jsonify({'message': "Command not recognized. Please start your command with '/gpt'."})
+
 
 if __name__ == '__main__':
     socketio.run(app,host='0.0.0.0', port=5000, debug=True)
